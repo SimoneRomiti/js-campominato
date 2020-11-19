69411d36c726d238212cf8bd5e0cfda03e2ea140
Simone Romiti <sr1992ei@gmail.com>
2020-11-19 17:40:33 +0100

    Campo minato con 5 prompt utente finito

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 // I numeri non possono essere duplicati.
-
-// FATTO
-
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
@@ -14,6 +11,7 @@
 // con difficoltà 0 => tra 1 e 100
 // con difficoltà 1 =>  tra 1 e 80
 // con difficoltà 2 => tra 1 e 50
+
 
 // FUNZIONE RICERCA DUPLICATO
 function ricerca(array, num){
@@ -37,10 +35,13 @@ function ricerca(array, num){
 
 // VARIABILI
 var arrayPc = [];
+var arrayUtente = [];
 var i = 0;
 var numero;
 var j;
-var controllo;
+var controlloDuplicatoPc;
+var controlloDuplicatoUtente = false;
+var esitoUtente;
 
 // GENERATORE ARRAY BOMBE PC SENZA DUPLICATI
 arrayPc[i] = Math.floor(Math.random() * (101 - 1) ) + 1;
@@ -50,9 +51,9 @@ for(i = 1; i < 16; i++){
 
   numero = Math.floor(Math.random() * (101 - 1) ) + 1;
 
-  controllo = ricerca(arrayPc, numero);
+  controlloDuplicatoPc = ricerca(arrayPc, numero);
 
-  if(controllo){
+  if(controlloDuplicatoPc){
     i = i - 1;
   } else {
     arrayPc[i] = numero;
@@ -61,3 +62,51 @@ for(i = 1; i < 16; i++){
 
 }
 console.log("Array Bombe: ", arrayPc);
+
+
+// UTENTE
+i = 0;
+j = 0;
+
+// CONTROLLO PRIMO NUMERO UTENTE SE PRESENTE IN ARRAY BOMBE PC
+arrayUtente[i] = parseInt(prompt("Inserisci un numero"));
+esitoUtente = ricerca(arrayPc, arrayUtente[i]);
+console.log(esitoUtente);
+
+if(esitoUtente){
+  alert("Hai Perso! Punteggio:" + " " + i);
+} else {
+  i = i + 1;
+  // CONTROLLO SU TUTTI GLI ALTRI NUMERI INSERITI SIA SE PRESENTI SU ARRAY BOMBE PC CHE SE DUPLICATI IN ARRAY UTENTE
+  while(i < 5 && esitoUtente == false && controlloDuplicatoUtente == false){
+
+    numeroUtente = parseInt(prompt("Inserisci un numero"));
+    // CONTROLLO DUPLICATO ARRAY UTENTE
+    controlloDuplicatoUtente = ricerca(arrayUtente, numeroUtente);
+    console.log("Controllo utente", controlloDuplicatoUtente);
+
+    if(controlloDuplicatoUtente){
+      alert("Non puoi inserire un numero già inserito!");
+      controlloDuplicatoUtente = false;
+      i = i - 1;
+      i++;
+    } else{
+
+      arrayUtente[i] = numeroUtente;
+      // CONTROLLO SE PRESENTE IN ARRAY BOMBE PC
+      esitoUtente = ricerca(arrayPc, arrayUtente[i]);
+      if(esitoUtente){
+        alert("Hai perso! Punteggio" + " " + i);
+        i++;
+      } else{
+
+        i++;
+      }
+    }
+
+  }
+  if(i >= 5 && esitoUtente == false){
+    alert("Hai vinto! Punteggio massimo:" + " " + i);
+  }
+
+}
